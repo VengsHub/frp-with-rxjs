e9ceb2c6f88e3fc28e70fde8df47867a4bd19285
mkoischwitz <mischa.koischwitz@akquinet.de>
2022-12-06 18:29:37 +0100

    implement threading issues example

--- src/app/threading-issues/threading-issues.component.ts
+++ src/app/threading-issues/threading-issues.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-threading-issues',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ThreadingIssuesComponent implements OnInit {
 
+  // rxjs
+  private _state = new BehaviorSubject<boolean>(true);
+  public state$: Observable<boolean> = this._state.asObservable();
+
   constructor() { }
 
   ngOnInit(): void {
+    const subscription = this.state$.subscribe(state => console.log('state change', state));
+    this._state.next(false);
+    setTimeout(() => {
+      subscription.unsubscribe();
+      console.log('unsubscribed');
+    }, 1000);
+    setInterval(() => this._state.next(Math.random() > 0.5), 2000);
   }
 
 }
