038c72723b5e168654839978d3d56ff1f4d14bf0
mkoischwitz <mischa.koischwitz@akquinet.de>
2022-12-28 19:23:44 +0100

    add click counter functionality for frp

--- src/app/drag-and-drop/drag-and-drop.component.ts
+++ src/app/drag-and-drop/drag-and-drop.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { fromEvent, scan } from 'rxjs';
 
 @Component({
   selector: 'app-drag-and-drop',
@@ -6,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./drag-and-drop.component.scss']
 })
 export class DragAndDropComponent implements OnInit {
-  constructor() { }
+
+  clickCounter = fromEvent(document, 'click')
+  .pipe(scan((count) => count + 1, 0))
+  .subscribe((count) => console.log(`Clicked ${count} times`));
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
